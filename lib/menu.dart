a65b78d7a6e8d0917b9d9891a0d46103e95dad1e
s-morgen <74549312+s-morgen@users.noreply.github.com>
2024-05-21 17:28:49 +0900

    メニュー　tabbar用画像追加

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 
+//画像引用
+//https://illustimage.com/
+
+//メニュー画面
 class  menuscreen extends StatelessWidget {
   const menuscreen ({super.key});
 
+  //選択tabの初期値
+  // int _currentPageIndex = 0;
+  //
+  // void _onItemTapped(int index){
+  //   //tapされたindexを_currentPageIndexに代入する
+  //   setState((){
+  //     _currentPageIndex = index;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-
     return  Scaffold(
       appBar: AppBar(
-        title: Text('あああ'),
+        centerTitle: true,
+        title:
+         TabBar(
+//           onTap: _onItemtapped,
+           tabs: [
+             Tab(text: '1'),
+             Tab(text: '2'),
+           ],
+           indicatorColor: Colors.black,
+         ),
+        backgroundColor: Color.fromRGBO(200,38,31,1),
       ),
 
     );
