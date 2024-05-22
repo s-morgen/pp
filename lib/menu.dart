58d6ff48959c734b61e49891963193be1311397b
s-morgen <74549312+s-morgen@users.noreply.github.com>
2024-05-22 15:08:25 +0900

    ラーメンタブの作成

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,39 +1,103 @@
 import 'package:flutter/material.dart';
 
-//画像引用
+//メニュー項目イラスト画像引用元
 //https://illustimage.com/
+
+//メニュー画像引用
+//https://www.itoyokado.co.jp/pp/menu/season/
 
 //メニュー画面
 class  menuscreen extends StatelessWidget {
   const menuscreen ({super.key});
 
-  //選択tabの初期値
-  // int _currentPageIndex = 0;
-  //
-  // void _onItemTapped(int index){
-  //   //tapされたindexを_currentPageIndexに代入する
-  //   setState((){
-  //     _currentPageIndex = index;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title:
-         TabBar(
-//           onTap: _onItemtapped,
-           tabs: [
-             Tab(text: '1'),
-             Tab(text: '2'),
-           ],
-           indicatorColor: Colors.black,
-         ),
-        backgroundColor: Color.fromRGBO(200,38,31,1),
-      ),
+    return  DefaultTabController(
+      initialIndex: 0, //最初のタブ
+      length: 6, //タブの数
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('メニュー',
+            style: TextStyle(
+                color: Color.fromRGBO(134,108,20, 1),),
+          ),
+          bottom: TabBar(
+            isScrollable: true, //スクロールを有効化
+            tabs: <Widget>[
+             Tab(
+               // child: Padding(
+               //   padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+               // ),
+               text: 'ラーメン',
+               icon: Image.asset('assets/menu/ramen-35px.png',
+                  fit: BoxFit.contain,
+                ),
+             ),
+             Tab(
+               text: 'チャーハン',
+               icon: Image.asset('assets/menu/mesi-35px.png',
+                 fit: BoxFit.contain,
+                 ),
+             ),
+             Tab(
+               text: '焼き物',
+               icon: Image.asset('assets/menu/yaki-35px.png'),
+             ),
+             Tab(
+               text: 'ポテト',
+               icon: Image.asset('assets/menu/poteto-35px.png'),
+             ),
+             Tab(
+               text: 'スイーツ',
+               icon: Image.asset('assets/menu/ice-35px.png'),
+             ),
+             Tab(
+               text: 'ドリンク',
+               icon: Image.asset('assets/menu/juice-35px.png'),
+             ),
+            ],
+             indicatorColor: Color.fromRGBO(134,108,20, 1),
+           ),
+          backgroundColor: Color.fromRGBO(250,196,16,1),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                      children: <Widget>[
+                        Container(
+                          child:
+                          Image.asset('assets/menu/ramen/pp-tonkotu-ramen.jpg',
+                            height: 300,
+                            //fit: BoxFit.contain,
+                          ),
+                        ),
+                        Text('とんこつラーメン'),
+                        Image.asset('assets/menu/ramen/pp-shoyu-ramen.jpg',
+                          height: 300,
+                        ),
+                        Text('しょうゆラーメン'),
+                        Image.asset('assets/menu/ramen.png',
+                          height: 300,
+                        ),
+                        Text('塩ラーメン'),
+                        Image.asset('assets/menu/ramen.png',
+                          height: 300,
+                        ),
+                        Text('みそラーメン'),
+                      ],
+                ),
 
+            ),
+            Center(
+              child: Text('22222'),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+
 }
